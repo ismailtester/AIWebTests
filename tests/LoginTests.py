0cad73fc029d52fe61e13e3398e8e54a5111d506
ismailtester <ismailtester@yandex.ru>
2025-06-27 01:03:01 +0300

    Добавил шаги к тестам и файл requirements

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,6 +1,7 @@
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
+import allure
 
 
 BASE_URL = "https://ok.ru/"
@@ -8,18 +9,20 @@ EMPTY_LOGIN_ERROR = "Введите логин"
 EMPTY_PASSWORD_ERROR = "Введите пароль"
 LOGIN = "ismailtester"
 
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки при пустой форме авторизации")
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
-
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
 
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки при пустом поле пароля")
 def test_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.input_login(LOGIN)
     LoginPage.click_login()
-
     assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
